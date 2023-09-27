cc03d32aa6a5d70d2bf62b0a1223803acebaf943
Debayan Pratihar <121422042+debayanpratihar@users.noreply.github.com>
2023-09-27 12:16:31 +0530

    last commit

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure("4d968a6b-b4bd-43f7-a82a-c6e365a9ac45");
   }, []);
 
   return null;
--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -4,26 +4,26 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const testimonials = [
   {
-    name: "Joel",
-    avatar: "J",
+    name: "Debayan Pratihar",
+    avatar: "D",
     title: "Software Engineer",
     description: "This is the best application I've ever used!",
   },
   {
-    name: "Antonio",
-    avatar: "A",
+    name: "Rupsha Ray",
+    avatar: "R",
     title: "Designer",
     description: "I use this daily for generating new photos!",
   },
   {
-    name: "Mark",
+    name: "Monami Mal",
     avatar: "M",
     title: "CEO",
     description: "This app has changed my life, cannot imagine working without it!",
   },
   {
-    name: "Mary",
-    avatar: "M",
+    name: "D Roy",
+    avatar: "D",
     title: "CFO",
     description: "The best in class, definitely worth the premium subscription!",
   },
